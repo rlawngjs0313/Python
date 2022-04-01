f63b73351f17b7367401c7e65ccce8017df0d53c
IlIllIlIIlll <75869755+IlIllIlIIlll@users.noreply.github.com>
2022-04-01 16:49:55 +0900

    컴수 과제

--- hw4_202210904.py
+++ hw4_202210904.py
@@ -1,3 +1,13 @@
-import numpy as np
-import matplotlib as pl
 
+import matplotlib as plo
+
+P = float(input("예치금을 입력하십시오. : "))
+T = int(input("예치 기간을 입력하십시오. : "))
+R = 0.03
+A1 = 0.0
+A = P * (1 + R)**T
+print(T, "년 후 총 금액은", A, "입니다.")
+
+for i in range(1, T+1) :
+    A1 += P * (1 + R)**i
+print(T, "년동안 모인 총 금액은", A1, "입니다.")

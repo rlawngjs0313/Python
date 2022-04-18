fd6b5000cb339c32a7f076929b3573f719cb5f60
IlIllIlIIlll <75869755+IlIllIlIIlll@users.noreply.github.com>
2022-04-19 04:52:30 +0900

    c언어

--- d.c
+++ d.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
 int main() {
-    int money = 0;
-    printf("주유에 필요한 금액을 입력하세요 : ");
-    scanf("%d", &money);
-    printf("%d로 주유할 수 있는 휘발유는 %.2f입니다.", money, money / 1995);
-    return 0;
+    
 }

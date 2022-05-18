81e23e8fa40b806095237e9f70a046e32ff0916d
IlIllIlIIlll <75869755+IlIllIlIIlll@users.noreply.github.com>
2022-05-19 01:49:28 +0900

    .

--- 백준.py
+++ 백준.py
@@ -1,6 +1,6 @@
-while True:
-    A, B = map(int, input().split())
-    if A == 0 and B == 0:
-        break
-    else:
+try:
+    while True:
+        A, B = map(int, input().split())
         print(A+B)
+except EOFError:
+    pass

cc1735edd94e30e0a248024c56fd4c798635480d
IlIllIlIIlll <75869755+IlIllIlIIlll@users.noreply.github.com>
2022-05-19 01:12:07 +0900

    .

--- 백준.py
+++ 백준.py
@@ -1,12 +1,6 @@
-A, B, C = list(map(int, input().split()))
-if A == B == C:
-    print(10000+A*1000)
-elif A == B or B == C or A == C:
-    if A == B:
-        print(1000+A*100)
-    elif B == C:
-        print(1000+C*100)
-    else:
-        print(1000+A*100)
-else:
-    print(max(A,B,C)*100)
+import sys
+
+A = int(input())
+for i in range(0,A):
+    B, C = map(int, sys.stdin.readline().split())
+    print(B+C)

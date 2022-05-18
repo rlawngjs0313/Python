3d5a2cd8fd4401d70f633ed1e3ec1a40c204e152
IlIllIlIIlll <75869755+IlIllIlIIlll@users.noreply.github.com>
2022-05-19 02:57:49 +0900

    .

--- 백준.py
+++ 백준.py
@@ -1,6 +1,16 @@
-try:
-    while True:
-        A, B = map(int, input().split())
-        print(A+B)
-except EOFError:
-    pass
+A = input()
+if int(A) < 10:
+    A = list(A)
+    A.append(0)
+    A.reverse()
+B = [0, 0]
+C = []
+B[0], B[1] = int(A[1]), int(A[0])+int(A[1])
+k = 1
+while int(A[0]) != B[0] or int(A[1]) != B[1]:
+    k += 1
+    for i in str(B[0]+B[1]):
+        C.append(i)
+    B[0], B[1] = B[1], int(C[-1])
+    C = []
+print(k)

1a760592e3b480310a41481f4027f1dde751fa3b
IlIllIlIIlll <75869755+IlIllIlIIlll@users.noreply.github.com>
2022-05-25 14:55:05 +0900

    .

--- 백준.py
+++ 백준.py
@@ -4,10 +4,17 @@ minus_num = 0
 while True:
     minus_num += cont
     if num == 1:
-        print(cont, minus_num)
+        print("1/1")
         break
     elif num - minus_num <= 0:
-        print(cont, minus_num)
+        if cont % 2 == 0:
+            A = range(1, cont+1)
+            B = range(cont, 0, -1)
+            print(f"{B[minus_num-num]}/{A[minus_num-num]}")
+        else:
+            A = range(cont, 0, -1)
+            B = range(1, cont+1)
+            print(f"{B[minus_num-num]}/{A[minus_num-num]}")
         break
     else:
         cont += 1

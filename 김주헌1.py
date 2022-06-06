43e4ab5abfacd8e9150639656f6905fedb91cfff
IlIllIlIIlll <75869755+IlIllIlIIlll@users.noreply.github.com>
2022-06-06 16:32:50 +0900

    기말고사 대비

---
+++ 김주헌1.py
@@ -0,0 +1,51 @@
+def inputing():
+    data_name = input("데이터 파일 이름을 입력하세요: ")
+    if data_name == '':
+        inputing()
+    return data_name
+def line_inputing():
+    find_lines = input("검색할 문자열을 입력하세요: ")
+    if find_lines == '':
+        line_inputing()
+    return find_lines
+def reading():
+    file_name = inputing()
+    try:
+        with open(file_name, "r", encoding="cp949") as f:
+            file_data = f.readlines()
+            for i in range(0, len(file_data)):
+                file_data[i] = file_data[i].strip()
+        return file_data
+    except FileNotFoundError:
+        print("파일을 찾을 수 없습니다.")
+    except UnicodeDecodeError:
+        with open(file_name, "r", encoding="utf-8") as f:
+            file_data = f.readlines()
+            for i in range(0, len(file_data)):
+                file_data[i] = file_data[i].strip()
+        return file_data
+def finding(data, lines):
+    result_data = []
+    cnt = []
+    counting = 0
+    for i in data:
+        counting += 1
+        new_data = str(i)
+        locate_word = []
+        startPos = 0
+        if new_data.find(lines) != -1:
+            while True:
+                if new_data.find(lines, startPos) != -1:
+                    locate_word.append(new_data.find(lines, startPos)+1)
+                    startPos += new_data.find(lines, startPos)+1
+                else:
+                    locate_word = tuple(locate_word)
+                    break
+            result_data.append(locate_word)
+            result_data.append(new_data)
+            cnt.append(counting)
+    return cnt, result_data
+result = finding(reading(), line_inputing())
+cnt, results = result[0], result[1]
+for i in range(len(cnt)):
+    print(f"{cnt[i]} : {results[2*i]} : {results[2*i+1]}")
--- 파이썬.py
+++
@@ -1,8 +0,0 @@
-f = open("test.txt", "r")
-lines = f.read()
-f.close()
-
-f2 = open(lines + "\\haha.txt", "r")
-print(lines+"\\haha.txt")
-for line in f2.readlines():
-    print(f2.readlines())

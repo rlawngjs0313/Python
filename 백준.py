e8406ced4815c8e245b7e0eedc60d6343d88c22c
IlIllIlIIlll <75869755+IlIllIlIIlll@users.noreply.github.com>
2022-06-27 22:46:05 +0900

    백준 10250풀이

--- 백준.py
+++ 백준.py
@@ -1,4 +1,14 @@
-A, B, C = map(int, input().split())
-day = 0
-if A > C:
-    day
+C = int(input())
+for i in range(C):
+    H, W, N = map(int, input().split())
+    H1 = int(N/H)+1
+    if H%W == 0:
+        if H1 < 10:
+            print(f"{H}0{H1}")
+        else:
+            print(f"{H}{H1}")
+    else:
+        if H1 < 10:
+            print(f"{N%H}0{H1}")
+        else:
+            print(f"{N%H}{H1}")

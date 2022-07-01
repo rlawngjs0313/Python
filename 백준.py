700ffd9505941858ceaa93e3587717bcf73e3824
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-01 19:43:58 +0900

    2839번

--- 백준.py
+++ 백준.py
@@ -1,14 +1,11 @@
-C = int(input())
-for k in range(C):
-    K = int(input())
-    N = int(input())
-    L1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-    L2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    for i in range(K):
-        for j in range(N):
-            if j == 0:
-                L2[j] = L1[j]
-            else:
-                L2[j] = L2[j-1] + L1[j]
-                L1[j] = L2[j]
-    print(L2.pop(N-1))
+N = int(input())
+cnt = 0
+while N >= 0:
+    if N%5 == 0:
+        cnt += (N//5)
+        print(cnt)
+        break
+    N -= 3
+    cnt += 1
+else:
+    print(-1)

a91ab97e71d2d9fb40ea45042e8d97e19cb412c9
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-01 20:44:28 +0900

    10757번

--- 백준.py
+++ 백준.py
@@ -1,11 +1,2 @@
-N = int(input())
-L1 = list(map(int, input().split()))
-cnt = 0
-
-for i in range(N):
-    for j in range(2, L1[i]+1):
-        if L1[i] == j:
-            cnt += 1
-        elif L1[i] % j == 0:
-            break
-print(cnt)
+A, B = map(int, input().split())
+print(A+B)

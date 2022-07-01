fd11b4aab2c304480f981b756dc7079689dda786
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-01 16:30:31 +0900

    10250번

--- 백준.py
+++ 백준.py
@@ -1,14 +1,15 @@
 C = int(input())
 for i in range(C):
     H, W, N = map(int, input().split())
-    H1 = int(N/H)+1
-    if H%W == 0:
-        if H1 < 10:
-            print(f"{H}0{H1}")
+    H1 = N%H
+    H2 = N//H+1
+    if H1 == 0:
+        if H2 <= 10:
+            print(f"{H}0{H2-1}")
         else:
-            print(f"{H}{H1}")
+            print(f"{H}{H2-1}")
     else:
-        if H1 < 10:
-            print(f"{N%H}0{H1}")
+        if H2 < 10:
+            print(f"{H1}0{H2}")
         else:
-            print(f"{N%H}{H1}")
+            print(f"{H1}{H2}")

c3952e9c8da8624024abc7b38c4be7107a640e13
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-01 20:43:28 +0900

    1978번

--- 백준.py
+++ 백준.py
@@ -1,14 +1,11 @@
-M = int(input())
 N = int(input())
-L1 = []
-for i in range(M, N+1):
-    for j in range(2, i+1):
-        if i == j:
-            L1.append(i)
-        elif i % j == 0:
+L1 = list(map(int, input().split()))
+cnt = 0
+
+for i in range(N):
+    for j in range(2, L1[i]+1):
+        if L1[i] == j:
+            cnt += 1
+        elif L1[i] % j == 0:
             break
-if L1 == []:
-    print(-1)
-else:
-    print(sum(L1))
-    print(min(L1))
+print(cnt)

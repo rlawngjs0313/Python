e86b3adcfb212f4e3ce58d4a783b1698036b8f35
rlawngjs0313 <juheon0313@naver.com>
2022-07-06 03:11:01 +0900

    1929번

--- 백준.py
+++ 백준.py
@@ -1,2 +1,14 @@
 M, N = map(int, input().split())
-L1 = list(range(M, N+1))
+a = [False] + [True] * (N + 1)
+m = int(N**0.5)
+
+for i in range(2, m + 1):
+    if a[i] == True:
+        for j in range(i + i, N + 1, i):
+            a[j] = False
+
+for i in range(M, N + 1):
+    if i == 1:
+        continue
+    if a[i] == True:
+        print(i)

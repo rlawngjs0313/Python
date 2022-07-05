84498a92c8d5ec2afd22294a281a3f545879c2fa
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-05 12:19:17 +0900

    .

--- 백준.py
+++ 백준.py
@@ -1,11 +1,2 @@
-M = int(input())
-while M > 1:
-    for i in range(2, M+1):
-        if M == i:
-            print(i, end="\n")
-            M = int(M/i)
-            break
-        elif M % i == 0:
-            print(i, end="\n")
-            M = int(M/i)
-            break
+M, N = map(int, input().split())
+L1 = list(range(M, N+1))

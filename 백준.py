6b4df5775a24780bcf04b50ff07bb2f33f48cd5a
rlawngjs0313 <juheon0313@naver.com>
2022-07-06 03:48:04 +0900

    4948번

--- 백준.py
+++ 백준.py
@@ -1,14 +1,12 @@
-M, N = map(int, input().split())
-a = [False] + [True] * (N + 1)
-m = int(N**0.5)
+N = int(input())
+while N > 0:
+    a = [False] + [True] * (2*N-1)
+    m = int((2*N)**0.5)
 
-for i in range(2, m + 1):
-    if a[i] == True:
-        for j in range(i + i, N + 1, i):
-            a[j] = False
-
-for i in range(M, N + 1):
-    if i == 1:
-        continue
-    if a[i] == True:
-        print(i)
+    for i in range(m + 1):
+        if a[i] == True:
+            for j in range(2*i+1, 2*N, i+1):
+                a[j] = False
+    a = a[N:]
+    print(a.count(True))
+    N = int(input())

bbc79272ac87c0bea60b15f83edb5d58aba65d18
rlawngjs0313 <juheon0313@naver.com>
2022-07-07 01:15:02 +0900

    .

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-A = int(input())
-B = input() # "123"
-
-a = B[0] # list, str, tuple ect
-b = B[1]
-c = B[2]
+a = int(input())
+if a%4 == 0:
+    if a%100 != 0 or a%400 == 0:
+        print('1')
+    else:
+        print('0')
+else:
+    print('0')

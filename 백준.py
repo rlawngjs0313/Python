311ac8aa5b36d8dee8bf6656e6aa5e3b8362b7d4
rlawngjs0313 <juheon0313@naver.com>
2022-07-06 20:06:03 +0900

    2447번

--- 백준.py
+++ 백준.py
@@ -1,15 +1,14 @@
-def um(num, cnt):
-    if cnt == 0:
-        print(f'{"____"*(num-cnt)}"재귀함수가 뭔가요?"')
-        print(f'{"____"*(num-cnt)}"재귀함수는 자기 자신을 호출하는 함수라네"')
-        print(f'{"____"*(num-cnt)}라고 답변하였지.')
-        return 0
-    print(f'{"____"*(num-cnt)}"재귀함수가 뭔가요?"')
-    print(f'{"____"*(num-cnt)}"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.')
-    print(f'{"____"*(num-cnt)}마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.')
-    print(f'{"____"*(num-cnt)}그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."')
-    um(num, cnt-1)
-    print(f'{"____"*(num-cnt)}라고 답변하였지.')
-num = int(input())
-print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
-um(num, num)
+N = int(input())
+def star(N):
+    if N == 3:
+        return ["***", "* *", "***"]
+    result = []
+    stars = star(N//3)
+    for i in stars:
+        result.append(i*3)
+    for i in stars:
+        result.append(i+' '*(N//3)+i)
+    for i in stars:
+        result.append(i*3)
+    return result
+print('\n'.join(star(N)))

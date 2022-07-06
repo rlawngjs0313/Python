5ac7aa6ef4553bf062c2fcafcd58acedbd7d63b8
rlawngjs0313 <juheon0313@naver.com>
2022-07-06 21:52:12 +0900

    2798번

--- 백준.py
+++ 백준.py
@@ -1,12 +1,10 @@
-def hano(n, f, t, a):
-    if n == 1:
-        L1.append(f"{f} {t}")
-        return
-    hano(n-1, f, a, t)
-    L1.append(f"{f} {t}")
-    hano(n-1, a, t, f)
-L1 = []
-N = int(input())
-hano(N, 1, 3, 2)
-print(len(L1))
-print('\n'.join(L1))
+N, M = map(int, input().split())
+L = list(map(int, input().split()))
+num = 0
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            newNum = (L[i]+L[j]+L[k])
+            if num <= newNum <= M:
+                num = newNum
+print(num)

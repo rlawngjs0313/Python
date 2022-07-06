b3fe31780a470149fffad4ac5d6fb52dbe6019bb
rlawngjs0313 <juheon0313@naver.com>
2022-07-07 00:59:40 +0900

    7568번

--- 백준.py
+++ 백준.py
@@ -1,12 +1,17 @@
 N = int(input())
-L1 = list(range(1, N+1))
-
-for i in L1:
-    result = i
-    for j in str(i):
-        result += int(j)
-    if result == N:
-        print(i)
-        break
-    elif i == N:
-        print(0)
+L1 = []
+result = []
+data = tuple(map(int, input().split()))
+L1.append(data)
+result.append(1)
+for i in range(N-1):
+    data = tuple(map(int, input().split()))
+    L1.append(data)
+    result.append(1)
+    for j in range(len(L1)-1):
+        if L1[j][0] > data[0] and L1[j][1] > data[1]:
+            result[-1] += 1
+        elif L1[j][0] < data[0] and L1[j][1] < data[1]:
+            result[j] += 1
+for i in result:
+    print(i, end=" ")

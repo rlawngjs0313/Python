c6c39bc3cbb1145869b37983b159f5067e9a42cb
rlawngjs0313 <juheon0313@naver.com>
2022-07-06 23:00:03 +0900

    2231번

--- 백준.py
+++ 백준.py
@@ -1,10 +1,12 @@
-N, M = map(int, input().split())
-L = list(map(int, input().split()))
-num = 0
-for i in range(N-2):
-    for j in range(i+1, N-1):
-        for k in range(j+1, N):
-            newNum = (L[i]+L[j]+L[k])
-            if num <= newNum <= M:
-                num = newNum
-print(num)
+N = int(input())
+L1 = list(range(1, N+1))
+
+for i in L1:
+    result = i
+    for j in str(i):
+        result += int(j)
+    if result == N:
+        print(i)
+        break
+    elif i == N:
+        print(0)

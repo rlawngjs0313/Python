3dbf37a4ca819f568792b95412a8e3d0d69167a4
rlawngjs0313 <juheon0313@naver.com>
2022-07-06 16:13:47 +0900

    10872번

--- 백준.py
+++ 백준.py
@@ -1,19 +1,8 @@
-def isPrime(n):
-    if n == 1:
-        return False
-    for j in range(2, int(n**0.5) + 1):
-        if n % j == 0:
-            return False
-    return True
-
-T = int(input())
-for i in range(T):
-    num = int(input())
-    a, b = num//2, num//2
-    while a > 0:
-        if isPrime(a) and isPrime(b):
-            print(a, b)
-            break
-        else:
-            a -= 1
-            b += 1
+def pac(num):
+    if num == 0:
+        return 1
+    if num <= 1:
+        return num
+    else:
+        return num * pac(num-1)
+print(pac(int(input())))

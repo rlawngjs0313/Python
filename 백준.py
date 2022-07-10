18603c1ea5bca44d2a439b41d1b4485cebb3e08a
rlawngjs0313 <juheon0313@naver.com>
2022-07-11 01:47:50 +0900

    11650번

--- 백준.py
+++ 백준.py
@@ -1,16 +1,7 @@
-import sys
-
 N = int(input())
-L1 = [0]*10000
-cnt = [0]*10000
-
+L1 = []
 for i in range(N):
-    data = int(sys.stdin.readline())
-    L1[data-1] = data
-    cnt[data-1] += 1
-for i in range(len(cnt)):
-    if cnt[i] == 0:
-        continue
-    else:
-        for j in range(cnt[i]):
-            sys.stdout.write(str(L1[i])+'\n')
+    L1.append(list(map(int, input().split())))
+L1.sort(key=lambda x: (x[0], x[1]))
+for i in L1:
+    print(i[0], i[1])

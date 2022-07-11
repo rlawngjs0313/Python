f5a33d5b00ca21e38f7833bb6d5afbd5ca09e8b2
rlawngjs0313 <juheon0313@naver.com>
2022-07-11 17:58:23 +0900

    10814번

--- 백준.py
+++ 백준.py
@@ -1,10 +1,9 @@
 import sys
 N = int(input())
-L1 = set([])
+L1 = []
 for i in range(N):
-    L1.add(sys.stdin.readline().rstrip())
-L1 = list(L1)
-L1.sort()
-L1.sort(key=len)
+    A, B = map(str, sys.stdin.readline().rstrip().split())
+    L1.append((int(A), B, i))
+L1.sort(key=lambda x : (x[0], x[2]))
 for i in L1:
-    print(i)
+    sys.stdout.write(f"{i[0]} {i[1]}"+'\n')

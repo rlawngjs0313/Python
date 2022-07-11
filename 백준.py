6a27cfa4befb869843b2dcadcd23ea80bbfad59e
rlawngjs0313 <juheon0313@naver.com>
2022-07-11 17:28:20 +0900

    1181번

--- 백준.py
+++ 백준.py
@@ -1,7 +1,10 @@
+import sys
 N = int(input())
-L1 = []
+L1 = set([])
 for i in range(N):
-    L1.append(list(map(int, input().split())))
-L1.sort(key=lambda x: (x[1], x[0]))
+    L1.add(sys.stdin.readline().rstrip())
+L1 = list(L1)
+L1.sort()
+L1.sort(key=len)
 for i in L1:
-    print(i[0], i[1])
+    print(i)

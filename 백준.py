f86edf5be1bb04a98b61311a26cb5500988f881b
rlawngjs0313 <juheon0313@naver.com>
2022-07-11 17:27:45 +0900

    11651번

--- 백준.py
+++ 백준.py
@@ -2,6 +2,6 @@ N = int(input())
 L1 = []
 for i in range(N):
     L1.append(list(map(int, input().split())))
-L1.sort(key=lambda x: (x[0], x[1]))
+L1.sort(key=lambda x: (x[1], x[0]))
 for i in L1:
     print(i[0], i[1])

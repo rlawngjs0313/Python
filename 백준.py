6d23a53b2eb30245ad27711aa5f2cf7b75d9142b
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-15 02:00:30 +0900

    10815번

--- 백준.py
+++ 백준.py
@@ -1,10 +1,20 @@
-import sys
-N = int(sys.stdin.readline())
-L1 = list(map(int, sys.stdin.readline().rstrip().split()))
-D1 = {}
-L2 = sorted(list(set(L1)))
-for i in range(len(L2)):
-    D1[L2[i]] = i
+N = int(input())
+L1 = list(map(int, input().split()))
+L1.sort()
 
-for i in L1:
-    sys.stdout.write(str(D1[i])+' ')
+def binarySearch(num):
+    l = 0
+    r = N-1
+    while l <= r :
+        index = (l+r)//2
+        if L1[index] == num :
+            return 1
+        elif L1[index] > num :
+            r = index - 1
+        else:
+            l = index + 1
+    return 0
+
+input()
+for i in list(map(int, input().split())):
+    print(binarySearch(i), end = ' ')

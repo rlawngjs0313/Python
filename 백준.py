e5edcb352f8e83f1e098fa5546981f58ed65c726
rlawngjs0313 <juheon0313@naver.com>
2022-07-17 03:16:10 +0900

    11478번

--- 백준.py
+++ 백준.py
@@ -1,5 +1,10 @@
 import sys
-N, M = map(int, input().split())
-S1 = set(map(int, sys.stdin.readline().split()))
-S2 = set(map(int, sys.stdin.readline().split()))
-print(len(S1.symmetric_difference(S2)))
+
+word = sys.stdin.readline().rstrip()
+S1 = set()
+N = len(word)
+
+for i in range(N):
+    for j in range(i, N):
+        S1.add(word[i:j+1])
+print(len(S1))

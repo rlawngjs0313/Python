6e22b46fdca7113d328fbf63af8e6a6d0765b138
rlawngjs0313 <juheon0313@naver.com>
2022-07-17 02:15:03 +0900

    10816번

--- 백준.py
+++ 백준.py
@@ -1,36 +1,16 @@
 import sys
 
-N, M = map(int, input().split())
-L1 = []
-L2 = []
-cnt = 0
-
-for i in range(N):
-    L1.append(sys.stdin.readline().rstrip())
-    
-L1.sort(key=len)
-
-for i in range(M):
-    L2.append(sys.stdin.readline().rstrip())
-
-def binarySearch(word):
-    l = 0
-    r = N-1
-    while l <= r :
-        index = (l+r)//2
-        if L1[index] == word:
-            return 1
-        elif len(L1[index]) > len(word):
-            r = index - 1
-        elif len(L1[index]) < len(word):
-            l = index + 1
-        elif len(L1[index]) == len(word):
-            if word in L1[l:r+1]:
-                return 1
-            else:
-                return 0
-    return 0
-
-for i in L2:
-    cnt += binarySearch(i)
-print(cnt)
+N = int(input())
+D1 = {}
+for i in list(map(int, sys.stdin.readline().split())):
+    if i in D1.keys():
+        D1[i] += 1
+    else:
+        D1[i] = 1
+M = int(input())
+L1 = map(int, sys.stdin.readline().split())
+for i in L1:
+    if i in D1.keys():
+        sys.stdout.write(f"{D1[i]}" + " ")
+    else:
+        sys.stdout.write("0" + " ")

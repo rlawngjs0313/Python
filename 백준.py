40bb945ff4025b4580a36da57cc477a8c55c50ef
rlawngjs0313 <juheon0313@naver.com>
2022-07-17 02:34:07 +0900

    1269번

--- 백준.py
+++ 백준.py
@@ -1,13 +1,5 @@
 import sys
-
 N, M = map(int, input().split())
-S1 = set()
-S2 = set()
-for i in range(N):
-    S1.add(sys.stdin.readline().rstrip())
-for i in range(M):
-    S2.add(sys.stdin.readline().rstrip())
-print(len(S1.intersection(S2)))
-result = sorted(S1.intersection(S2))
-for i in result:
-    sys.stdout.write(f"{i}" + "\n")
+S1 = set(map(int, sys.stdin.readline().split()))
+S2 = set(map(int, sys.stdin.readline().split()))
+print(len(S1.symmetric_difference(S2)))

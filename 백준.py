7637316d3f542905d96e5bc98c319287907b3ee4
rlawngjs0313 <juheon0313@naver.com>
2022-07-17 03:50:30 +0900

    10828번

--- 백준.py
+++ 백준.py
@@ -1,10 +1,26 @@
 import sys
 
-word = sys.stdin.readline().rstrip()
-S1 = set()
-N = len(word)
+N = int(sys.stdin.readline())
+St1 = []
 
 for i in range(N):
-    for j in range(i, N):
-        S1.add(word[i:j+1])
-print(len(S1))
+    data = list(sys.stdin.readline().split())
+    if data[0] == "push":
+        St1.append(data[1])
+    elif data[0] == "pop":
+        if len(St1) == 0:
+            print(-1)
+        else:
+            print(St1.pop())
+    elif data[0] == "size":
+        print(len(St1))
+    elif data[0] == "empty":
+        if len(St1) == 0:
+            print(1)
+        else:
+            print(0)
+    elif data[0] == "top":
+        if len(St1) == 0:
+            print(-1)
+        else:
+            print(St1[-1])

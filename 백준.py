2fa24236f793c4a365495264fa9f88589b0792ef
rlawngjs0313 <juheon0313@naver.com>
2022-07-17 00:59:01 +0900

    14425번

--- 백준.py
+++ 백준.py
@@ -1,20 +1,36 @@
-N = int(input())
-L1 = list(map(int, input().split()))
-L1.sort()
+import sys
 
-def binarySearch(num):
+N, M = map(int, input().split())
+L1 = []
+L2 = []
+cnt = 0
+
+for i in range(N):
+    L1.append(sys.stdin.readline().rstrip())
+    
+L1.sort(key=len)
+
+for i in range(M):
+    L2.append(sys.stdin.readline().rstrip())
+
+def binarySearch(word):
     l = 0
     r = N-1
     while l <= r :
         index = (l+r)//2
-        if L1[index] == num :
+        if L1[index] == word:
             return 1
-        elif L1[index] > num :
+        elif len(L1[index]) > len(word):
             r = index - 1
-        else:
+        elif len(L1[index]) < len(word):
             l = index + 1
+        elif len(L1[index]) == len(word):
+            if word in L1[l:r+1]:
+                return 1
+            else:
+                return 0
     return 0
 
-input()
-for i in list(map(int, input().split())):
-    print(binarySearch(i), end = ' ')
+for i in L2:
+    cnt += binarySearch(i)
+print(cnt)

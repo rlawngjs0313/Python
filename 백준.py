f861105761f1334d45d1bf763b3e7bc8168ce9e1
rlawngjs0313 <juheon0313@naver.com>
2022-07-18 04:23:45 +0900

    1107번

--- 백준.py
+++ 백준.py
@@ -1,26 +1,15 @@
-N, r, c = map(int, input().split())
-cnt = 0
-while N > 1:
-    size = (2 ** N) // 2
-    if r < size and c < size:
-        pass
-    elif r < size and c >= size:
-        cnt += size ** 2
-        c -= size
-    elif r >= size and c < size:
-        cnt += size ** 2 * 2
-        r -= size
-    elif r >= size and c >= size:
-        cnt += size ** 2 * 3
-        r -= size
-        c -= size
-    N -= 1
+N = int(input())
+M = int(input())
+L1 = []
+result = abs(100 - N)
 
-if r == 0 and c == 0:
-    print(cnt)
-if r == 0 and c == 1:
-    print(cnt + 1)
-if r == 1 and c == 0:
-    print(cnt + 2)
-if r == 1 and c == 1:
-    print(cnt + 3)
+if M != 0:
+    L1 = list(input().split())
+
+for i in range(1000001): 
+    for j in str(i):
+        if j in L1:
+            break
+    else:
+        result = min(result, len(str(i)) + abs(i - N))
+print(result)

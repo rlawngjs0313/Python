0cacf15ea06bfaac28d8802e6aee45d7df8103d4
rlawngjs0313 <juheon0313@naver.com>
2022-07-18 02:37:02 +0900

    1012번

--- 백준.py
+++ 백준.py
@@ -1,18 +1,32 @@
 import sys
+sys.setrecursionlimit(1000000)
 
-N1 = [1, 0]
-result = [1, 1]
-index = [0,0]
+cnt = 0
 
-N = int(input())
-for i in range(N):
-    data = int(sys.stdin.readline())
-    if data == 0:
-        sys.stdout.write(f"{N1[0]} {N1[1]}" + "\n")
-    elif data == 1:
-        sys.stdout.write(f"{N1[1]} {N1[0]}" + "\n")
-    else:
-        for i in range(data-2):
-            result[0], result[1] = result[1], (result[0]+result[1])
-        sys.stdout.write(f"{result[0]} {result[1]}" + "\n")
-    result = [1, 1]
+def dfs(x, y):
+    if x < 0 or x >= M or y < 0 or y >= N:
+        return
+    if graph[y][x] == 0:
+        return
+    graph[y][x] = 0
+
+    dfs(x+1, y)
+    dfs(x-1, y)
+    dfs(x, y+1)
+    dfs(x, y-1)
+
+T = int(sys.stdin.readline())
+for i in range(T):
+    M, N, K = map(int, sys.stdin.readline().rstrip().split())
+    graph = [[0]*M for k in range(N)]
+    for j in range(K):
+        data = list(map(int, sys.stdin.readline().split()))
+        graph[data[1]][data[0]] = 1
+    
+    for j in range(M):
+        for k in range(N):
+            if graph[k][j] == 1:
+                dfs(j, k)
+                cnt += 1
+    sys.stdout.write(f"{cnt}" + "\n")
+    cnt = 0

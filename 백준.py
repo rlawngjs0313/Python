e91d877896bfb41e20ad0dfb33bb330f5a4a5867
JuHeon0313 <juheon0313@naver.com>
2022-07-17 14:48:53 +0900

    9012번

--- 백준.py
+++ 백준.py
@@ -1,13 +1,27 @@
 import sys
-
-N = int(input())
+N = int(sys.stdin.readline())
+L1 = []
+bl = False
 
 for i in range(N):
-    data = list(map(str, sys.stdin.readline().rstrip().split()))
+    data = sys.stdin.readline().rstrip()
     for j in data:
-        data = list(map(str, j))
-        data.reverse()
-        for k in data:
-            sys.stdout.write(f"{k}")
-        sys.stdout.write(" ")
-    sys.stdout.write("\n")
+        if j == "(":
+            L1.append(1)
+        elif j == ")":
+            if L1 == []:
+                sys.stdout.write("NO" + "\n")
+                bl = True
+                break
+            else:
+                L1.pop()
+    if bl:
+        L1 = []
+        bl = False
+        continue
+    if L1 == []:
+        sys.stdout.write("YES" + "\n")
+    else:
+        sys.stdout.write("NO" + "\n")
+    L1 = []
+    bl = False

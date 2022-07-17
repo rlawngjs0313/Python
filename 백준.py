c6cd7620bd6f113e0b37036b94eb71d36aa4bd05
JuHeon0313 <juheon0313@naver.com>
2022-07-17 16:32:34 +0900

    2920번

--- 백준.py
+++ 백준.py
@@ -1,32 +1,7 @@
-import sys
-
-N = int(input())
-L1 = list(range(1, N+1))
-L2 = []
-result = []
-index = 0
-
-for i in range(N):
-    data = int(sys.stdin.readline())
-    if L2 == []:
-        while True:
-            L2.append(L1.pop(0))
-            result.append("+")
-            if L2[-1] == data:
-                index = L2.pop()
-                result.append("-")
-                break
-    elif L2[-1] <= data:
-        while L2[-1] != data:
-            L2.append(L1.pop(0))
-            result.append("+")
-        index = L2.pop()
-        result.append("-")
-    elif L2[-1] > data:
-        result.clear()
-        result.append("NO")
-        for j in range(N-i-1):
-            sys.stdin.readline()
-        break
-for i in result:
-    sys.stdout.write(i + "\n")
+data = list(map(int, input().split()))
+if data == [1,2,3,4,5,6,7,8]:
+    print("ascending")
+elif data == [8,7,6,5,4,3,2,1]:
+    print("descending")
+else:
+    print("mixed")

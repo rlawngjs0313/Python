e393e2e46d5d782f8f772d49567c65e82f75170d
JuHeon0313 <juheon0313@naver.com>
2022-07-17 19:15:35 +0900

    10845번

--- 백준.py
+++ 백준.py
@@ -1,9 +1,31 @@
-while True:
-    L1 = list(map(int, input().split()))
-    L1.sort()
-    if L1[0] == L1[1] == L1[2] == 0:
-        break
-    if L1[0]**2 + L1[1]**2 == L1[2]**2:
-        print("right")
-    else:
-        print("wrong")
+import sys
+
+N = int(input())
+L1 = []
+
+for i in range(N):
+    data = list(map(str, sys.stdin.readline().rstrip().split()))
+    if data[0] == 'push':
+        L1.append(int(data[1]))
+    elif data[0] == "pop":
+        if L1 == []:
+            print(-1)
+        else:
+            print(L1.pop(0))
+    elif data[0] == "size":
+        print(len(L1))
+    elif data[0] == "empty":
+        if L1 == []:
+            print(1)
+        else:
+            print(0)
+    elif data[0] == "front":
+        if L1 == []:
+            print(-1)
+        else:
+            print(L1[0])
+    elif data[0] == "back":
+        if L1 == []:
+            print(-1)
+        else:
+            print(L1[-1])

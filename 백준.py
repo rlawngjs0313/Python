77eaeb41bb0bb7ca9d656db3bd64e8aad58b6a67
JuHeon0313 <juheon0313@naver.com>
2022-07-17 19:19:09 +0900

    10866번

--- 백준.py
+++ 백준.py
@@ -5,13 +5,20 @@ L1 = []
 
 for i in range(N):
     data = list(map(str, sys.stdin.readline().rstrip().split()))
-    if data[0] == 'push':
+    if data[0] == 'push_front':
+        L1.insert(0, int(data[1]))
+    elif data[0] == 'push_back':
         L1.append(int(data[1]))
-    elif data[0] == "pop":
+    elif data[0] == "pop_front":
         if L1 == []:
             print(-1)
         else:
             print(L1.pop(0))
+    elif data[0] == "pop_back":
+        if L1 == []:
+            print(-1)
+        else:
+            print(L1.pop())
     elif data[0] == "size":
         print(len(L1))
     elif data[0] == "empty":

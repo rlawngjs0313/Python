8925e92257716aaf652aac4ba35dd4f39df4e3ac
JuHeon0313 <juheon0313@naver.com>
2022-07-17 12:51:45 +0900

    9093번

--- 백준.py
+++ 백준.py
@@ -1,26 +1,13 @@
 import sys
 
-N = int(sys.stdin.readline())
-St1 = []
+N = int(input())
 
 for i in range(N):
-    data = list(sys.stdin.readline().split())
-    if data[0] == "push":
-        St1.append(data[1])
-    elif data[0] == "pop":
-        if len(St1) == 0:
-            print(-1)
-        else:
-            print(St1.pop())
-    elif data[0] == "size":
-        print(len(St1))
-    elif data[0] == "empty":
-        if len(St1) == 0:
-            print(1)
-        else:
-            print(0)
-    elif data[0] == "top":
-        if len(St1) == 0:
-            print(-1)
-        else:
-            print(St1[-1])
+    data = list(map(str, sys.stdin.readline().rstrip().split()))
+    for j in data:
+        data = list(map(str, j))
+        data.reverse()
+        for k in data:
+            sys.stdout.write(f"{k}")
+        sys.stdout.write(" ")
+    sys.stdout.write("\n")

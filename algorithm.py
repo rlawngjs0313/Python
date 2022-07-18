61e9dbd1cf6a94a57412bbea489705ed85d85140
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-19 03:00:54 +0900

    DFS, BFS 알고리즘 추가

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,56 @@
-# DFS
-def dfs(x, y):
-    if x < 0 or x >= M or y < 0 or y >= N:
-        return
-    if graph[y][x] == 0:
-        return
-    graph[y][x] = 0
-    # 탐색한 노드 지우기
+import sys
+import copy
+sys.setrecursionlimit(1000000)
 
-    dfs(x+1, y)
-    dfs(x-1, y)
-    dfs(x, y+1)
-    dfs(x, y-1)
-    # 상하좌우 탐색
-M = "X축"
-N = "Y축"
-graph = "트리, 그래프"
+N, M, V = map(int, input().split())
+graph_DFS = [[] for _ in range(N+1)]
+graph_BFS = []
+result_DFS = [V, ]
+result_BFS = [V, ]
+temp = []
+
+def DFS(node):
+    if len(result_DFS) == N:
+        return
+    if graph_DFS[node] == []:
+        return
+    for i in range(len(graph_DFS[node])):
+        N1 = min(graph_DFS[node])
+        graph_DFS[node].remove(N1)
+        if N1 in result_DFS:
+            continue
+        result_DFS.append(N1)
+        DFS(N1)
+
+def BFS(node):
+    if len(result_BFS) == N:
+        return
+    if graph_BFS[node] == []:
+        return
+    for i in range(len(graph_BFS[node])):
+        N1 = min(graph_BFS[node])
+        graph_BFS[node].remove(N1)
+        if N1 in result_BFS:
+            continue
+        result_BFS.append(N1)
+        temp.append(N1)
+    
+    for i in range(len(temp)):
+        if temp:
+            BFS(temp.pop(0))
+    
+
+for i in range(M):
+    data = list(map(int, sys.stdin.readline().split()))
+    graph_DFS[data[0]].append(data[1])
+    graph_DFS[data[1]].append(data[0])
+
+graph_BFS = copy.deepcopy(graph_DFS)
+DFS(V)
+result_DFS = str(result_DFS).replace('[', '').replace(']', '').replace(',', '')
+BFS(V)
+result_BFS = str(result_BFS).replace('[', '').replace(']', '').replace(',', '')
+print(result_DFS)
+print(result_BFS)
+
+# DFS, BFS 알고리즘 / 다녀간 노드 순서 출력

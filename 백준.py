77e757128b327b1c7c7b1e23a5035281e4d8d333
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-19 18:48:26 +0900

    1676번

--- 백준.py
+++ 백준.py
@@ -1,19 +1,10 @@
-data = input().split("-")
-result1 = 0
-result2 = 0
+from math import factorial
 
-if len(data) == 1:
-    data = list(map(int, data[0].split("+")))
-    for i in data:
-        result1 += i
-    print(result1)
-else:
-    data1 = list(map(int, data[0].split("+")))
-    for i in data1:
-        result1 += i
-    for i in range(len(data[1:])):
-        data[i+1] = sum(list(map(int, data[i+1].split("+"))))
-    result2 = sum(data[1:])
-    data.clear()
-    data.append(str(result1-result2))
-    print(data[0])
+
+num = str(factorial(int(input())))
+cnt = 0
+
+while num.endswith("0"):
+    cnt += 1
+    num = num.removesuffix("0")
+print(cnt)

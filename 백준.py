a1eaf57fbdd0cf4582ba9c0ae3d4995917618ae9
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-19 20:53:44 +0900

    1697

--- 백준.py
+++ 백준.py
@@ -1,10 +1,21 @@
-from math import factorial
+import sys
+from collections import deque
+input = sys.stdin.readline()
 
+def BFS(node):
+    result = [0]*(100001)
+    queue = deque()
+    queue.append(node)
 
-num = str(factorial(int(input())))
-cnt = 0
+    while queue:
+        A = queue.popleft()
+        if A == K:
+            print(result[A])
+            break
+        for i in (A-1, A+1, 2*A):
+            if 0 <= i <= 100000 and not result[i]:
+                result[i] = result[A] + 1
+                queue.append(i)
 
-while num.endswith("0"):
-    cnt += 1
-    num = num.removesuffix("0")
-print(cnt)
+N, K = map(int, input.split())
+BFS(N)

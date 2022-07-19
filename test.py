df54f2539e84d204be9c0273c98847b4ec1cc072
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-19 21:39:35 +0900

    .

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
-N = int(input())
-L1 = []
+max = 10001
+N1 = set(range(1, max)) # (1,2,3,4,5,6,...,10000) 집합
+N2 = set() # 집합
 
-for i in range(N):
-    L1.append(int(input()))
+for i in range(1, max):
+    temp = i
+    for j in str(i):
+        temp += int(j)
+    N2.add(temp) # 39, 51, 57, ...
 
-cnt, location = [0] * max(L1), [0] * max(L1)
-
+# 셀프 넘버
+N1 = N1.difference(N2) # 집합요소인 셀프 넘버들
+N1 = list(N1) # 정렬 위해 리스트
+N1.sort() # 오름차순으로 / .replace()
+N1 = str(N1).replace("[", "").replace("]", "").replace(", ", "\n") # O(n) => O(1)
+print(N1)

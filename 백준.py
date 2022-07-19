203a41a0cb27f9b6e210a255f185833f65fc16ee
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-19 22:43:35 +0900

    11279번

--- 백준.py
+++ 백준.py
@@ -8,8 +8,8 @@ for i in range(N):
     data = int(sys.stdin.readline())
     if data == 0:
         if len(L1) != 0:
-            print(heapq.heappop(L1))
+            print(-1*(heapq.heappop(L1)))
         else:
             print(0)
     else:
-        heapq.heappush(L1, data)
+        heapq.heappush(L1, data*(-1))

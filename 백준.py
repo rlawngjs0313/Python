79235a9daac56f2b67ccc4e57be633c62e5a2436
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-19 22:31:25 +0900

    1927번

--- 백준.py
+++ 백준.py
@@ -1,21 +1,15 @@
 import sys
-from collections import deque
-input = sys.stdin.readline()
+import heapq
 
-def BFS(node):
-    result = [0]*(100001)
-    queue = deque()
-    queue.append(node)
+N = int(input())
+L1 = []
 
-    while queue:
-        A = queue.popleft()
-        if A == K:
-            print(result[A])
-            break
-        for i in (A-1, A+1, 2*A):
-            if 0 <= i <= 100000 and not result[i]:
-                result[i] = result[A] + 1
-                queue.append(i)
-
-N, K = map(int, input.split())
-BFS(N)
+for i in range(N):
+    data = int(sys.stdin.readline())
+    if data == 0:
+        if len(L1) != 0:
+            print(heapq.heappop(L1))
+        else:
+            print(0)
+    else:
+        heapq.heappush(L1, data)

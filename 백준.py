a4d413dabbf4d83c05adbc01fb9039b69915fa2e
rlawngjs0313 <juheon0313@naver.com>
2022-07-21 01:31:19 +0900

    2606번

--- 백준.py
+++ 백준.py
@@ -1,15 +1,35 @@
+from collections import deque
 import sys
-import heapq
+sys.setrecursionlimit(1000000)
 
+def BFS():
+    visited = [1]
+    queue = deque()
+    queue.append(1)
+
+    while queue:
+        A = queue.popleft()
+        for i in graph[A]:
+            if i not in visited:
+                visited.append(i)
+                queue.append(i)
+
+    return visited
+
+K = int(input())
 N = int(input())
-L1 = []
+graph = [[] for _ in range(K+1)]
 
 for i in range(N):
-    data = int(sys.stdin.readline())
-    if data == 0:
-        if len(L1) != 0:
-            print(-1*(heapq.heappop(L1)))
-        else:
-            print(0)
-    else:
-        heapq.heappush(L1, data*(-1))
+    data = list(map(int, sys.stdin.readline().split()))
+    graph[data[0]].append(data[1])
+    graph[data[1]].append(data[0])
+
+visited = BFS()
+cnt = 0
+
+for i in range(2, K+1):
+    if i in visited:
+        cnt += 1
+
+print(cnt)

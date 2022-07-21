f08b4ab4cac5de38c5c67fda8f870d882f4c5dd7
rlawngjs0313 <juheon0313@naver.com>
2022-07-21 17:26:51 +0900

    5525번 50점

--- 백준.py
+++ 백준.py
@@ -2,30 +2,18 @@ import sys
 sys.setrecursionlimit(1000000)
 
 N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+S = sys.stdin.readline().rstrip()
+word = "IO"*N + "I"
+index = 1
+cnt = 0
 
-for i in range(N):
-    cnt = 0
-    p = sys.stdin.readline().rstrip()
-    n = int(sys.stdin.readline())
-    if n == 0:
-        sys.stdin.readline()
-        data = []
+while True:
+    result = S.find(word, index-1)
+    if result != -1:
+        cnt += 1
+        index = result + 3
     else:
-        data = list(map(int, sys.stdin.readline().rstrip().rstrip("]").lstrip("[").split(",")))
-    for j in p:
-        if j == "R":
-            cnt += 1
-        if j == "D":
-            if data == []:
-                sys.stdout.write("error" + '\n')
-                break
-            else:
-                if cnt == 0 or cnt % 2 == 0:
-                    data.pop(0)
-                else:
-                    data.pop(-1)
-    else:
-        if cnt == 0 or cnt % 2 == 0:
-            sys.stdout.write(str(data).replace(" ", "") + '\n')
-        else:
-            sys.stdout.write(str(list(reversed(data))).replace(" ", "") + '\n')
+        break
+
+print(cnt)

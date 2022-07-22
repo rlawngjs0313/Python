b4056b0027a4c02f9c59e05ecd9ca0b3fcf48cb0
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-22 21:13:40 +0900

    1406번

--- 백준.py
+++ 백준.py
@@ -1,14 +1,31 @@
+from collections import deque
 import sys
 
-N, M = map(int, sys.stdin.readline().split())
-data = list(map(int, sys.stdin.readline().split()))
-L1 = [0]
-temp = 0
+data = list(sys.stdin.readline().rstrip())
+N = int(sys.stdin.readline())
+L1 = data
+L2 = deque([])
 
-for i in data:
-    temp += i
-    L1.append(temp)
-
-for i in range(M):
-    index = list(map(int, sys.stdin.readline().split()))
-    sys.stdout.write(str(L1[index[1]] - L1[index[0]-1]) + '\n')
+for i in range(N):
+    cmd = list(map(str, sys.stdin.readline().split()))
+    if cmd[0] == "L":
+        if L1 == []:
+            continue
+        L2.appendleft(L1.pop())
+        continue
+    if cmd[0] == "D":
+        if L2 == deque():
+            continue
+        L1.append(L2.popleft())
+        continue
+    if cmd[0] == "B":
+        if L1 == []:
+            continue
+        L1.pop()
+        continue
+    if cmd[0] == "P":
+        L1.append(cmd[1])
+        continue
+L1.append(list(L2))
+L1 = str(L1).replace("[", "").replace("]", "").replace("'", "").replace(", ", "")
+sys.stdout.write(L1)

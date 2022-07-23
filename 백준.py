17410a3faa59f137b6f996755fd7ac2313a3b10a
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-23 17:48:57 +0900

    1002번

--- 백준.py
+++ 백준.py
@@ -1,31 +1,18 @@
-from collections import deque
 import sys
 
-data = list(sys.stdin.readline().rstrip())
-N = int(sys.stdin.readline())
-L1 = data
-L2 = deque([])
+T = int(sys.stdin.readline())
 
-for i in range(N):
-    cmd = list(map(str, sys.stdin.readline().split()))
-    if cmd[0] == "L":
-        if L1 == []:
-            continue
-        L2.appendleft(L1.pop())
-        continue
-    if cmd[0] == "D":
-        if L2 == deque():
-            continue
-        L1.append(L2.popleft())
-        continue
-    if cmd[0] == "B":
-        if L1 == []:
-            continue
-        L1.pop()
-        continue
-    if cmd[0] == "P":
-        L1.append(cmd[1])
-        continue
-L1.append(list(L2))
-L1 = str(L1).replace("[", "").replace("]", "").replace("'", "").replace(", ", "")
-sys.stdout.write(L1)
+for i in range(T):
+    x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
+    d = ((x1-x2)**2+(y1-y2)**2)**0.5
+    if (x1, y1) == (x2, y2) and r1 == r2:
+        if r1 == 0:
+            sys.stdout.write("1" + '\n')
+        else:
+            sys.stdout.write("-1" + '\n')
+    elif d > r1+r2 or d < abs(r1-r2):
+        sys.stdout.write("0" + '\n')
+    elif d == (r1+r2) or d == (abs(r1-r2)):
+        sys.stdout.write("1" + '\n')
+    elif d < r1+r2 or d > abs(r1-r2):
+        sys.stdout.write("2" + '\n')

849a3302ef9b4e52e73de3c14931a5f680abf439
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-24 17:26:27 +0900

    4375번

--- 백준.py
+++ 백준.py
@@ -1,18 +1,13 @@
 import sys
-
-T = int(sys.stdin.readline())
-
-for i in range(T):
-    x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
-    d = ((x1-x2)**2+(y1-y2)**2)**0.5
-    if (x1, y1) == (x2, y2) and r1 == r2:
-        if r1 == 0:
-            sys.stdout.write("1" + '\n')
-        else:
-            sys.stdout.write("-1" + '\n')
-    elif d > r1+r2 or d < abs(r1-r2):
-        sys.stdout.write("0" + '\n')
-    elif d == (r1+r2) or d == (abs(r1-r2)):
-        sys.stdout.write("1" + '\n')
-    elif d < r1+r2 or d > abs(r1-r2):
-        sys.stdout.write("2" + '\n')
+try:
+    while True:
+        data = int(sys.stdin.readline())
+        i = 0
+        while True:
+            i += 1
+            test = "1"*i
+            if int(test) % data == 0:
+                sys.stdout.write(str(i) + '\n')
+                break
+except:
+    exit()

1e8beeabdcc0cb3c8e1de678495252dfab64d914
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-25 19:30:10 +0900

    17413번

--- 백준.py
+++ 백준.py
@@ -1,13 +1,50 @@
 import sys
-from collections import deque
 
-N, K = map(int, sys.stdin.readline().split())
-L1 = deque(list(range(1, N+1)))
+data = sys.stdin.readline().rstrip()
 result = []
+temp = ""
+isTag = False
 
-while len(L1) != 0:
-    for i in range(K-1):
-        data = L1.popleft()
-        L1.append(data)
-    result.append(L1.popleft())
-print(str(result).replace("[", "<").replace("]", ">"))
+for i in data:
+    if isTag:
+        if i == ">":
+            temp += i
+            result.append(temp)
+            isTag = False
+            temp = ""
+            continue
+        else:
+            temp += i
+    elif i == "<":
+        if temp:
+            result.append(temp)
+            temp = "<"
+            isTag = True
+            continue
+        else:
+            temp += i
+            isTag = True
+    elif i == " " and temp:
+        result.append(temp)
+        temp = ""
+        continue
+    else:
+        temp += i
+if temp:
+    result.append(temp)
+
+for i in range(len(result)):
+    if "<" in result[i]:
+        sys.stdout.write(str(result[i]))
+        isTag = True
+    else:
+        temp = list(result[i])
+        temp.reverse()
+        if isTag:
+            sys.stdout.write(str(temp).replace("[", "").replace("]", "").replace(", ", "").replace("'", ""))
+            isTag = False
+        else:
+            if i == 0:
+                sys.stdout.write(str(temp).replace("[", "").replace("]", "").replace(", ", "").replace("'", ""))
+            else:
+                sys.stdout.write(" " + str(temp).replace("[", "").replace("]", "").replace(", ", "").replace("'", ""))

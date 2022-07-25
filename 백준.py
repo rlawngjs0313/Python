fc6f6a782207709ebf66863febe4fc1a7470858f
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-25 15:30:07 +0900

    1158번

--- 백준.py
+++ 백준.py
@@ -1,6 +1,13 @@
 import sys
+from collections import deque
 
-N = int(sys.stdin.readline())
-data = list(map(int, sys.stdin.readline().split()))
-data.sort()
-print(data[0]*data[-1])
+N, K = map(int, sys.stdin.readline().split())
+L1 = deque(list(range(1, N+1)))
+result = []
+
+while len(L1) != 0:
+    for i in range(K-1):
+        data = L1.popleft()
+        L1.append(data)
+    result.append(L1.popleft())
+print(str(result).replace("[", "<").replace("]", ">"))

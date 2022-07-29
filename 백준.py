2c17c82f3d7786f31f3169e8d7a2df853c565175
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-29 19:42:45 +0900

    11655번

--- 백준.py
+++ 백준.py
@@ -1,1 +1,9 @@
-print(len(input()))
+data = input()
+for i in data:
+    if i.isalpha():
+        if 65 <= ord(i) <= 96:
+            print(chr(65+((ord(i)+13-65)%26)), end="")
+        else:
+            print(chr(97+((ord(i)+13-97)%26)), end="")
+    else:
+        print(i, end="")

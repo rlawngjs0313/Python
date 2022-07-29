2d5d44407761a6c4779187daa2fd12a8f827a5d2
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-29 19:02:54 +0900

    10808번

--- 백준.py
+++ 백준.py
@@ -1,24 +1,8 @@
 import sys
 
-data = list(input())
-L1 = []
+data = sys.stdin.readline().rstrip()
+result = [0] * 26
+
 for i in data:
-    if i.isalpha():
-        sys.stdout.write(i)
-    else:
-        if i == '(':
-            L1.append(i)
-        elif i == '*' or i == '/':
-            while L1 and (L1[-1] == '*' or L1[-1] =='/'):
-                sys.stdout.write(L1.pop())
-            L1.append(i)
-        elif i == '+' or i == '-':
-            while L1 and L1[-1] != '(':
-                sys.stdout.write(L1.pop())
-            L1.append(i)
-        elif i == ')':
-            while L1 and L1[-1] != '(':
-                sys.stdout.write(L1.pop())
-            L1.pop()
-while L1 :
-    sys.stdout.write(L1.pop())
+    result[ord(i)-ord("a")] += 1
+print(*result)

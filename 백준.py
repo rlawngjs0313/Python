36788cc7153fe3717e58d5f8f77fdd89c91265bf
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-29 16:30:31 +0900

    17298번

--- 백준.py
+++ 백준.py
@@ -1,20 +1,12 @@
-import sys
+N = int(input())
+numbers = list(map(int, input().split()))
 
-data = list(map(str, sys.stdin.readline().rstrip()))
-L1 = []
-result = 0
-isLaser = False
+stack = []
+answer =[-1] * N
 
-for i in range(len(data)):
-    if isLaser:
-        isLaser = False
-        continue
-    if data[i] == "(" and data[i+1] == ")":
-        result += len(L1)
-        isLaser = True
-    elif data[i] == "(":
-        L1.append(data[i])
-    elif data[i] == ")":
-        L1.pop()
-        result += 1
-print(result)
+for i in range(N):
+    while stack and numbers[stack[-1]] < numbers[i]:
+        answer[stack.pop()] = numbers[i]
+    stack.append(i)
+
+print(*answer)

3803865a66115713eb263751faa97656a24a904b
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-29 19:30:25 +0900

    2743번

--- 백준.py
+++ 백준.py
@@ -1,16 +1,1 @@
-try:
-    while True:
-        data = input()
-        result = [0] * 4
-        for i in data:
-            if 'A'<=i<='Z':
-                result[1] += 1
-            elif 'a'<=i<='z':
-                result[0] += 1
-            elif i.isnumeric():
-                result[2] += 1
-            elif i == " ":
-                result[3] += 1
-        print(*result)
-except:
-    exit()
+print(len(input()))

26dfb4c6a38dee6f333e07ff8bca4554494ebc7d
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-29 19:09:58 +0900

    10820번

--- 백준.py
+++ 백준.py
@@ -1,8 +1,16 @@
-import sys
-
-data = sys.stdin.readline().rstrip()
-result = [0] * 26
-
-for i in data:
-    result[ord(i)-ord("a")] += 1
-print(*result)
+try:
+    while True:
+        data = input()
+        result = [0] * 4
+        for i in data:
+            if 'A'<=i<='Z':
+                result[1] += 1
+            elif 'a'<=i<='z':
+                result[0] += 1
+            elif i.isnumeric():
+                result[2] += 1
+            elif i == " ":
+                result[3] += 1
+        print(*result)
+except:
+    exit()

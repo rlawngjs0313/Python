8ec84647d7db257001155b1d04f8f24f38b405f8
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-07-31 20:43:51 +0900

    2004번

--- 백준.py
+++ 백준.py
@@ -1,19 +1,12 @@
 import sys
 
-L1 = [False]+[False]+[True]*(999999)
+def solution(N, M):
+    cnt = 0
+    while N != 0:
+        N = N//M
+        cnt += N
+    return cnt
 
-for i in range(1000):
-    if L1[i] == True:
-        for j in range(i*2, len(L1)-1, i):
-            L1[j] = False
+n, m = map(int, sys.stdin.readline().split())
 
-while True:
-    n = int(sys.stdin.readline())
-    index = 0
-    if n == 0:
-        break
-    else:
-        for i in range(3, 1000001):
-            if L1[i] and L1[n-i]:
-                sys.stdout.write(f"{n} = {i} + {n-i}" + '\n')
-                break
+print(min(solution(n, 2) - solution(m, 2) - solution(n-m, 2), solution(n, 5) - solution(m, 5) - solution(n-m, 5)))

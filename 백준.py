1df541a215da12d901a0d784ec751b898d6c0f97
rlawngjs0313 <juheon0313@naver.com>
2022-08-02 04:55:27 +0900

    1373번

--- 백준.py
+++ 백준.py
@@ -1,12 +1,4 @@
 import sys
 
-N, S = map(int, sys.stdin.readline().split())
-data = list(map(lambda x: abs(x-S), list(map(int, sys.stdin.readline().split()))))
-
-for i in range(len(data)-1):
-    m, n = max(data[i], data[i+1]), min(data[i], data[i+1])
-    while n != 0:
-        temp = m%n
-        m, n = n, temp
-    data[i+1] = abs(m)
-print(data[-1])
+data = "0b" + sys.stdin.readline().rstrip()
+print(oct(int(data, 2)).replace("0o", ""))

ae939a7ec4375ea84893756217362ecbf4788cc3
rlawngjs0313 <juheon0313@naver.com>
2022-08-02 04:41:01 +0900

    17087번

--- 백준.py
+++ 백준.py
@@ -1,17 +1,12 @@
 import sys
-from collections import deque
 
-T = int(sys.stdin.readline())
+N, S = map(int, sys.stdin.readline().split())
+data = list(map(lambda x: abs(x-S), list(map(int, sys.stdin.readline().split()))))
 
-for i in range(T):
-    data = deque(map(int, sys.stdin.readline().split()))
-    n = data.popleft()
-    result = 0
-    for i in range(n):
-        for j in range(i+1, n):
-            M, N = max(data[i], data[j]), min(data[i], data[j])
-            while N != 0:
-                temp = M%N
-                (M,N) = (N,temp)
-            result += abs(M)
-    sys.stdout.write(str(result) + '\n')
+for i in range(len(data)-1):
+    m, n = max(data[i], data[i+1]), min(data[i], data[i+1])
+    while n != 0:
+        temp = m%n
+        m, n = n, temp
+    data[i+1] = abs(m)
+print(data[-1])

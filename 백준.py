d3cf82f63fc27a42fdb3e0f716565437ef1ff8fa
JuHeon <rlawngjs0313@naver.com>
2022-09-14 05:39:17 +0900

    9095번

--- 백준.py
+++ 백준.py
@@ -1,4 +1,14 @@
 import sys
+sys.setrecursionlimit(1000000)
 
-data = "0o" + sys.stdin.readline().rstrip()
-print(bin(int(data, 8)).replace("0b", ""))
+def solution(n):
+    if n == 1: return 1
+    elif n == 2: return 2
+    elif n == 3: return 4
+    else: return solution(n-1) + solution(n-2) + solution(n-3)
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    n = int(sys.stdin.readline())
+    sys.stdout.write(str(solution(n)) + '\n')

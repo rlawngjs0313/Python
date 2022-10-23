f81a4acb844e287e616f124eca8b3ef94a2c417f
JuHeon <rlawngjs0313@naver.com>
2022-10-23 23:29:50 +0900

    1789번

--- 백준.py
+++ 백준.py
@@ -1,18 +1,15 @@
-import sys
-
-N = int(input())
-result = 0
-init = [0,0]
-L1 = []
-
-for i in range(N):
-    L1.append(list(map(int, sys.stdin.readline().split())))
-
-L1.sort(key= lambda x: (x[1], x[0]))
-
-for i in L1:
-    if i[0] >= init[1]:
-        result += 1
-        init = i[:]
-
-print(result)
+S = int(input())
+cnt = 1
+if S == 1:
+    print(1)
+else:
+    while True:
+        S -= cnt
+        if S == 0:
+            print(cnt)
+            break
+        elif S < 0:
+            print(cnt-1)
+            break
+        else:
+            cnt += 1

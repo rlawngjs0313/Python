891697c378efac0c51ebec14b6cc9099555f8ef0
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-10-28 22:50:21 +0900

    완전한 괄호 체크 문제

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-i = 0
-print(i, "d")
+import sys
+
+queue = []
+data = sys.stdin.readline()
+
+for i in data:
+    try:
+        if i in "[{(":
+            queue.append(i)
+        elif i == ')' and queue[-1] == '(':
+            queue.pop()
+        elif i == '}' and queue[-1] == '{':
+            queue.pop()
+        elif i == ']' and queue[-1] == '[':
+            queue.pop()
+    except:
+        print("False")
+        exit()
+if queue != []:
+    print("False")
+else:
+    print("True")

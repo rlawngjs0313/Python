e897ba56fbad2a02b52da7ba9703f978d6be7bca
JuHeon <rlawngjs0313@naver.com>
2022-10-28 22:01:47 +0900

    .

--- 백준.py
+++ 백준.py
@@ -1,39 +1,26 @@
 import sys
-
-def three(idx): # 3개 묶어서 사기
-    global result
-    temp = min(data[idx: idx + 3])
-    data[idx:idx+3] = [data[idx]-temp, data[idx+1]-temp, data[idx+2]-temp]
-    result += 7 * temp
+import numpy as np
 
 
-def two(idx): # 2개 묶어서 사기
-    global result
-    temp = min(data[idx: idx + 2])
-    data[idx:idx+2] = [data[idx]-temp, data[idx+1]-temp]
-    result += 5 * temp
+T = int(sys.stdin.readline())
 
+for i in range(T):
+    U_D = [["w", "w", "w"] for _ in range(3)]
+    D_U = [["y", "y", "y"] for _ in range(3)]
+    F_B = [["r", "r", "r"] for _ in range(3)]
+    B_F = [["o", "o", "o"] for _ in range(3)]
+    L_R = [["g", "g", "g"] for _ in range(3)]
+    R_L = [["b", "b", "b"] for _ in range(3)]
 
-def one(idx): # 1개 사기
-    global result
-    result += 3 * data[idx]
-
-
-N = int(input())
-L1 = [0 for _ in range(N)]
-data = list(map(int, sys.stdin.readline().split())) + [0, 0]
-result = 0
-
-for i in range(N):
-    if data[i+1] > data[i+2]: # 만약 2번째가 3번째보다 크면
-        temp = min(data[i], data[i+1] - data[i+2])          #
-        data[i:i+2] = [data[i]-temp, data[i+1]-temp]        # 2개 먼저 묶어서 사기
-        result += 5 * temp                                  #
-        three(i)    # 그 다음에 3개 묶어서 사기
-    else:   # 그 이외에는
-        three(i)    # 3개 먼저 묶어서 사고
-        two(i)  # 2개 묶어서 사기
-    one(i)  # i에 존재하는 갯수 사기
-    if data == L1: # 만약 다 샀다면
-        break   # 빠르게 종료
-print(result)
+    n = int(sys.stdin.readline())
+    execution = list(map(str, sys.stdin.readline().split()))
+    for j in execution:
+        if j == "U-":
+            F_B[0][:], R_L[0][:], B_F[0][:], L_R[0][:], U_D[:][:
+                                                               ] = L_R[0][:], F_B[0][:], R_L[0][:], B_F[0][:]
+        elif j == "U+":
+            F_B[0][:], L_R[0][:], B_F[0][:], R_L[0][:] = R_L[0][:], B_F[0][:], L_R[0][:], F_B[0][:]
+        elif j == "D-":
+            F_B[2][:], R_L[2][:], B_F[2][:], L_R[2][:] = L_R[2][:], F_B[2][:], R_L[2][:], B_F[2][:]
+        elif j == "D+":
+            F_B[2][:], L_R[2][:], B_F[2][:], R_L[2][:] = R_L[2][:], B_F[2][:], L_R[2][:], F_B[2][:]

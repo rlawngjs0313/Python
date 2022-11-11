c1a3d7704b8a9eb6cec43f2952d778ecee1c2696
JuHeon <rlawngjs0313@naver.com>
2022-11-11 15:21:55 +0900

    12015번 (python3는 시간초과, PyPy3로 해야함)

--- 백준.py
+++ 백준.py
@@ -1,13 +1,28 @@
-import sys  # 1946번 신입 사원
+import sys
 
-T = int(input())
+N = int(input())
+A = list(map(int, sys.stdin.readline().split()))
+sortedA = []
+lastNum = 0
 
-for _ in range(T):
-    N = int(input())
-    mx, result = 0, 0
-    L1, temp = [], []
-    for j in range(N):
-        L1.append(list(map(int, sys.stdin.readline().split())))
-    L1.sort(key=lambda x: (x[0]))
-    # 엄
-    print(result)
+for i in A:
+    if i > lastNum:
+        sortedA.append(i)
+        lastNum = i
+    else:
+        start = 0
+        end = len(sortedA)-1
+        while start <= end:
+            mid = (end + start) // 2
+            if i == sortedA[mid]:
+                break
+            elif i > sortedA[mid]:
+                start = mid + 1
+            else:
+                end = mid - 1
+        if i <= sortedA[mid]:
+            sortedA[mid] = i
+        else:
+            sortedA[mid+1] = i
+        lastNum = sortedA[-1]
+print(len(sortedA))

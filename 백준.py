8af20466afc872e9a829aa62c753d934844c5415
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-11-13 01:44:27 +0900

    14002번 LIS, 이분탐색 O(nlogn)

--- 백준.py
+++ 백준.py
@@ -16,3 +16,10 @@ for i, j in zip(A[1:], range(1, N+1)):
     dp[j] = Idx
 
 print(length)
+result = []
+for i in range(N-1, -1, -1):
+    if dp[i] == length - 1:
+        result.append(A[i])
+        length -= 1
+result.reverse()
+print(*result)

c33e9d9500367d8bb25e1f9a612a88c9f8857373
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-11-13 02:35:02 +0900

    1946번 주석

--- 백준.py
+++ 백준.py
@@ -11,7 +11,7 @@ for i in range(T):
     data.sort(key=lambda x: (x[0]))
     last = 0
     for j in range(N-1):
-        if data[j+1][1] < data[last][1]:
-            last = j + 1
+        if data[j+1][1] < data[last][1]:    # 1차 점수로 정렬 한뒤, 2차 점수를 비교 / 만약 점수가 낮다면 둘 다 떨어지니 불합
+            last = j + 1                    # 합격했다면 비교 점수를 그 점수로 설정 / 이 점수보다 떨어진다면 둘 다 떨어지는 경우
             result += 1
     print(result)

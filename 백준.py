c00e1a0a10cd12804a578b8ed3fbc3ee3ddea4a8
JuHeon <rlawngjs0313@naver.com>
2022-12-14 01:24:30 +0900

    1715번

--- 백준.py
+++ 백준.py
@@ -1,32 +1,20 @@
 import sys
-from collections import deque
+from heapq import heappop, heappush
 
+N = int(input())
 L1 = []
-N = int(sys.stdin.readline())
 result = 0
-
-for i in range(N):
-    L1.append(int(sys.stdin.readline()))
 
 if N == 1:
     print(0)
     exit()
 
-L1.sort()
-L1 = deque(L1)
-temp = L1.popleft() + L1.popleft()
-L2 = [temp]
-L1.appendleft(temp)
+for i in range(N):
+    heappush(L1, int(sys.stdin.readline()))
 
-while len(L1) > 2:
-    if L1[0] + L1[1] >= L1[1] + L1[2]:
-        tp = L1.popleft()
-        temp = L1.popleft() + L1.popleft()
-        L1.appendleft(tp)
-    else:
-        temp = L1.popleft() + L1.popleft()
-    L2.append(temp)
-    L1.appendleft(temp)
+while len(L1) != 1:
+    temp = heappop(L1) + heappop(L1)
+    heappush(L1, temp)
+    result += temp
 
-L2.append(sum(L1))
-print(sum(L2))
+print(result)

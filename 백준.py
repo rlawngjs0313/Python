ca6a939d33ea343a0a33f3bd4d2d457c32c1eb21
JuHeon <rlawngjs0313@naver.com>
2022-12-14 18:44:23 +0900

    1744번

--- 백준.py
+++ 백준.py
@@ -1,20 +1,39 @@
 import sys
+from heapq import heappop, heappush
 
-A, B = map(int, sys.stdin.readline().split())
+N = int(input())
+L1 = []
+L2 = []
+L3 = []
 result = 0
 
-while A != B and A < B:
-    if B % 2 == 0:
-        B //= 2
-        result += 1
-    elif str(B)[-1] == '1':
-        B //= 10
-        result += 1
+for i in range(N):
+    temp = int(sys.stdin.readline())
+    if temp < 0:
+        heappush(L1, temp)
+    elif temp > 1:
+        heappush(L2, -1*temp)
     else:
-        print(-1)
-        exit()
+        heappush(L3, temp)
 
-if A > B:
-    print(-1)
-else:
-    print(result+1)
+while L2 != []:
+    temp = -1*heappop(L2)
+    if L2 != []:
+        temp *= -1*heappop(L2)
+        result += temp
+    else:
+        result += temp
+
+while L1 != []:
+    temp = heappop(L1)
+    if L1 != []:
+        temp *= heappop(L1)
+        result += temp
+    else:
+        if L3 != [] and L3[0] == 0:
+            heappop(L3)
+        else:
+            result += temp
+
+result += sum(L3)
+print(result)

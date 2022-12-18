9c60b552abdff8130fbecc752b467894554167bc
IlIllIlIIlll <75869755+rlawngjs0313@users.noreply.github.com>
2022-12-18 22:27:52 +0900

    1700번

--- 백준.py
+++ 백준.py
@@ -2,16 +2,28 @@ import sys  # 1700번 도전
 
 N, K = map(int, sys.stdin.readline().split())
 L1 = list(map(int, sys.stdin.readline().split()))
-L2 = set(L1[:N])
+L2 = []
 result = 0
 
-for i in range(N, K, N):
-    temp = set(L1[i:i+N])
-    A, B = L2.difference(temp), temp.difference(L2)
-    if len(A) != 0 and len(B) != 0:
-        result += len(B)
-        for j, k in zip(A, B):
-            L2.add(k)
-            L2.remove(j)
+while len(L2) != N and L1 != []:
+    temp = L1.pop(0)
+    if temp not in L2:
+        L2.append(temp)
+
+while L1 != []:
+    i = L1.pop(0)
+    if i not in L2:
+        L3 = []
+        cnt = 0
+        for j in L2:
+            if j in L1:
+                L3.append([-1*(L1.index(j)), cnt])
+            else:
+                L3.append([-(K+1), cnt])
+            cnt += 1
+        L3.sort(key=lambda x: (x[0]))
+        del L2[L3[0][1]]
+        L2.append(i)
+        result += 1
 
 print(result)

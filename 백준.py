581ab2b09b2124775dc6f7e0d4fd5653f4d89296
JuHeon <rlawngjs0313@naver.com>
2023-01-02 20:45:34 +0900

    17420번 40점

--- 백준.py
+++ 백준.py
@@ -1,4 +1,5 @@
 import sys
+from heapq import heapify, heappop, heappush
 
 N = int(input())
 L1 = []
@@ -7,12 +8,22 @@ result = 0
 temp1 = list(map(int, sys.stdin.readline().split()))
 temp2 = list(map(int, sys.stdin.readline().split()))
 for i in range(N):
-    L1.append([temp1[i], temp2[i]])
-
-L1.sort(key=lambda x:(x[1], x[0]))
+    heappush(L1, [temp2[i], temp1[i]])
 
 while L1:
+    if L1[0][1] < L1[0][0]:
+        x = (L1[0][0] - L1[0][1]) // 30
+        if (L1[0][0] - L1[0][1]) % 30 != 0:
+            x += 1
+        L1[0][1] += 30*x
+        result += x
     for i in L1:
-        
+        if L1[0][1] >= i[1] and L1[0][0] < i[0]:
+            x = (L1[0][1] - i[1]) // 30
+            if (L1[0][1] - i[1]) % 30 != 0:
+                x += 1
+            i[1] += 30*x
+            result += x 
+    heappop(L1)
 
 print(result)

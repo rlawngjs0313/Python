ff1ca298e247db003530bdc73818c42e479aad74
rlawngjs0313 <juheon0313@naver.com>
2023-01-03 19:17:25 +0900

    1339번

--- 백준.py
+++ 백준.py
@@ -1,29 +1,19 @@
 import sys
-from heapq import heapify, heappop, heappush
 
 N = int(input())
-L1 = []
+L1 = [0 for _ in range(26)]
 result = 0
 
-temp1 = list(map(int, sys.stdin.readline().split()))
-temp2 = list(map(int, sys.stdin.readline().split()))
 for i in range(N):
-    heappush(L1, [temp2[i], temp1[i]])
+    data = sys.stdin.readline().rstrip()
+    length = len(data)-1
+    for j in data:
+        L1[ord(j) % 65] += 10**length
+        length -= 1
 
-while L1:
-    if L1[0][1] < L1[0][0]:
-        x = (L1[0][0] - L1[0][1]) // 30
-        if (L1[0][0] - L1[0][1]) % 30 != 0:
-            x += 1
-        L1[0][1] += 30*x
-        result += x
-    for i in L1:
-        if L1[0][1] >= i[1] and L1[0][0] < i[0]:
-            x = (L1[0][1] - i[1]) // 30
-            if (L1[0][1] - i[1]) % 30 != 0:
-                x += 1
-            i[1] += 30*x
-            result += x 
-    heappop(L1)
+L1.sort(reverse=True)
+
+for i, j in zip(L1, range(9, 0, -1)):
+    result += i*j
 
 print(result)

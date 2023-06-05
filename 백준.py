42a44c4d04660e6a8060156ec19798ece9bb8ef6
JuHeon <75869755+rlawngjs0313@users.noreply.github.com>
2023-06-05 08:01:36 +0000

    1969번

--- 백준.py
+++ 백준.py
@@ -1,14 +1,21 @@
-S = input()
-T = list(input())
+import sys
 
-while len(T) != len(S):
-    if T[-1] == 'A':
-        T.pop()
-    else:
-        T.pop()
-        T.reverse()
+N, M = map(int, sys.stdin.readline().split())
+L1 = [[] for _ in range(M)]
+result = []
+result_count = 0
 
-if T == list(S):
-    print(1)
-else:
-    print(0)
+for _ in range(N):
+    data = sys.stdin.readline().rstrip()
+    for idx, value in enumerate(data):
+        L1[idx].append(value)
+
+for i in range(M):
+    L1[i].sort()
+    result.append(max(L1[i], key=L1[i].count))
+    while result[i] in L1[i]:
+        L1[i].remove(result[i])
+    result_count += (len(L1[i]))
+
+print(str(result).replace("[", "").replace("]", "").replace("'", "").replace(", ", ""))
+print(result_count)

384420294add9513d9b137c7abc4b1fc6583a7ed
JuHeon <75869755+rlawngjs0313@users.noreply.github.com>
2023-07-28 14:16:03 +0000

    9251번 (LCS)

--- 백준.py
+++ 백준.py
@@ -1,21 +1,11 @@
-import sys
+A = input()
+B = input()
+LCS = [[0 for _ in range(len(A)+1)] for _ in range(len(B)+1)]
 
-N, M = map(int, sys.stdin.readline().split())
-L1 = [[] for _ in range(M)]
-result = []
-result_count = 0
-
-for _ in range(N):
-    data = sys.stdin.readline().rstrip()
-    for idx, value in enumerate(data):
-        L1[idx].append(value)
-
-for i in range(M):
-    L1[i].sort()
-    result.append(max(L1[i], key=L1[i].count))
-    while result[i] in L1[i]:
-        L1[i].remove(result[i])
-    result_count += (len(L1[i]))
-
-print(str(result).replace("[", "").replace("]", "").replace("'", "").replace(", ", ""))
-print(result_count)
+for Bidx, Bvalue in enumerate(B):
+    for Aidx, Avalue in enumerate(A):
+        if Bvalue == Avalue:
+            LCS[Bidx+1][Aidx+1] = LCS[Bidx][Aidx]+1
+        else:
+            LCS[Bidx+1][Aidx+1] = max(LCS[Bidx][Aidx+1], LCS[Bidx+1][Aidx])
+print(LCS[-1][-1])

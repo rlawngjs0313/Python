d0799a09304e7b35854094878bde2c9e6fed1830
JuHeon <75869755+rlawngjs0313@users.noreply.github.com>
2023-08-15 12:21:44 +0000

    1912번

--- 백준.py
+++ 백준.py
@@ -1,13 +1,8 @@
-import sys
 N = int(input())
+L1 = list(map(int, input().split()))
+DP = [L1[0]]
 
-DP1 = [0, 0, 0]
-DP2 = [0, 0, 0]
-for i in range(N):
-    R, G, B = map(int, sys.stdin.readline().split())
-    DP2[0] = min(R+DP1[1], R+DP1[2])
-    DP2[1] = min(G+DP1[0], G+DP1[2])
-    DP2[2] = min(B+DP1[0], B+DP1[1])
-    if i != N-1:
-        DP1[:] = DP2[:]
-print(min(DP2))
+for i in L1[1:]:
+    DP.append(max(DP[-1]+i, i))
+
+print(max(DP))

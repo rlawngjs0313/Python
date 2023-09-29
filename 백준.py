7432f372886ba8921dc6b07a48e8c0d85c4921a2
JuHeon <75869755+rlawngjs0313@users.noreply.github.com>
2023-09-29 06:51:13 +0000

    1966번 구현

--- 백준.py
+++ 백준.py
@@ -1,22 +1,26 @@
-def solution(today, terms, privacies):
-    D1 = {}
-    result = []
-    for i in terms:
-        D1[i[0]] = int(i[2:])
-    
-    for idx, i in enumerate(privacies):
-        year = int(i[:4])
-        mon = int(i[5:7])
-        day = int(i[8:10])
-        exe = i[11]
-        
-        year += (mon + D1[exe]) // 12
-        mon = (mon + D1[exe]) % 12
-        
-        date = (year-2000)*12*28 + mon*28 + day
-        todayDate = (int(today[:4])-2000)*12*28 + int(today[5:7])*28 + int(today[8:])
-        if date <= todayDate:
-            result.append(idx+1)
-    return result
+# 1966번 구현문제 23:57
 
-print(solution("2022.05.19", ["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]))
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    L1 = list(map(int, input().split()))
+    result = 0
+    maxNum = max(L1)
+
+    while True:
+        if L1[0] == maxNum:
+            if M == 0:
+                print(result+1)
+                break
+            else:
+                L1.pop(0)
+                maxNum = max(L1)
+                result += 1
+                M -= 1
+        else:
+            L1.append(L1.pop(0))
+            if M != 0:
+                M -= 1
+            else:
+                M = len(L1)-1

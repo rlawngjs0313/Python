5e3779a9352acc321a02e4bad6ba581121377548
JuHeon <75869755+rlawngjs0313@users.noreply.github.com>
2023-09-29 06:23:48 +0000

    정리

--- 백준.py
+++ 백준.py
@@ -1,20 +1,22 @@
-import sys
+def solution(today, terms, privacies):
+    D1 = {}
+    result = []
+    for i in terms:
+        D1[i[0]] = int(i[2:])
+    
+    for idx, i in enumerate(privacies):
+        year = int(i[:4])
+        mon = int(i[5:7])
+        day = int(i[8:10])
+        exe = i[11]
+        
+        year += (mon + D1[exe]) // 12
+        mon = (mon + D1[exe]) % 12
+        
+        date = (year-2000)*12*28 + mon*28 + day
+        todayDate = (int(today[:4])-2000)*12*28 + int(today[5:7])*28 + int(today[8:])
+        if date <= todayDate:
+            result.append(idx+1)
+    return result
 
-N = int(input())
-L1 = [0]
-DP = [0 for _ in range(N+1)]
-
-for _ in range(N):
-    L1.append(int(sys.stdin.readline()))
-
-if N == 1:
-    print(L1[-1])
-    exit()
-
-DP[1] = L1[1]
-DP[2] = DP[1] + L1[2]
-
-for i in range(3, N+1):
-    DP[i] = max(DP[i-3]+L1[i-1], DP[i-2]) + L1[i]
-
-print(DP[-1])
+print(solution("2022.05.19", ["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]))

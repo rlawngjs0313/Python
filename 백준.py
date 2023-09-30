20c1b8211795ebc2e9ac33b5945164ca9263192b
JuHeon <75869755+rlawngjs0313@users.noreply.github.com>
2023-09-30 04:41:06 +0000

    1475번 구현

--- 백준.py
+++ 백준.py
@@ -1,18 +1,29 @@
-#2563 구현 약 30:00
+#1475 구현 24:55
 
-N = int(input())
-L1 = [[0 for _ in range(101)] for _ in range(101)]
+from collections import deque
+
+N = input()
+L1 = deque([])
+for i in N:
+    L1.append(i)
 result = 0
 
-for _ in range(N):
-    w, h = map(int, input().split())
-    duple = 0
-    for i in range(10):
-        for j in range(10):
-            if L1[w+i][h+j] != 0:
-                duple += 1
-            L1[w+i][h+j] += 1
+while L1 != deque([]):
+    result += 1
+    L2 = [1 for _ in range(10)]
+    for _ in range(len(L1)):
+        if L2[int(L1[0])] != 0:
+            L2[int(L1[0])] -= 1
+            L1.popleft()
+        elif L1[0] == "6" and L2[9] != 0:
+            L1.popleft()
+            L2[9] -= 1
+        elif L1[0] == "9" and L2[6] != 0:
+            L1.popleft()
+            L2[6] -= 1
+        else:
+            temp = L1.popleft()
+            L1.append(temp)
 
-    result += (100 - duple)
 
 print(result)

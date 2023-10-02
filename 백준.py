a021fa1062173144bdeb0bb8dad5c1daa9e06d6c
JuHeon <75869755+rlawngjs0313@users.noreply.github.com>
2023-10-02 07:05:52 +0000

    성격 유형 검사하기

--- 백준.py
+++ 백준.py
@@ -1,29 +1,29 @@
-#1475 구현 24:55
+# 2022 카카오 인턴쉽 / 성격 유형 검사하기 / 11:57
 
-from collections import deque
-
-N = input()
-L1 = deque([])
-for i in N:
-    L1.append(i)
-result = 0
-
-while L1 != deque([]):
-    result += 1
-    L2 = [1 for _ in range(10)]
-    for _ in range(len(L1)):
-        if L2[int(L1[0])] != 0:
-            L2[int(L1[0])] -= 1
-            L1.popleft()
-        elif L1[0] == "6" and L2[9] != 0:
-            L1.popleft()
-            L2[9] -= 1
-        elif L1[0] == "9" and L2[6] != 0:
-            L1.popleft()
-            L2[6] -= 1
-        else:
-            temp = L1.popleft()
-            L1.append(temp)
-
-
-print(result)
+def solution(survey, choices):
+    D1 = {"A":0,"N":0,"R":0,"T":0,"C":0,"F":0,"J":0,"M":0}
+    result = []
+    for i, j in zip(survey, choices):
+        if j < 4:
+            D1[i[0]] += 4-j
+        elif j > 4:
+            D1[i[1]] += j-4
+    
+    if D1["R"] >= D1["T"]:
+        result.append("R")
+    else:
+        result.append("T")
+    if D1["C"] >= D1["F"]:
+        result.append("C")
+    else:
+        result.append("F")
+    if D1["J"] >= D1["M"]:
+        result.append("J")
+    else:
+        result.append("M")
+    if D1["A"] >= D1["N"]:
+        result.append("A")
+    else:
+        result.append("N")
+    
+    return "".join(result)

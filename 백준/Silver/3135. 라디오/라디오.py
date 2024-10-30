428ffa2b6a9ac1aeed98f1bc4c5ea2024fa29063
JuHeon <75869755+rlawngjs0313@users.noreply.github.com>
2024-10-30 19:05:59 +0900

    [Silver V] Title: 라디오, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/3135. 라디오/라디오.py
@@ -0,0 +1,20 @@
+import sys, bisect
+input = sys.stdin.readline
+print = sys.stdout.write
+
+A, B = map(int, input().split())
+N = int(input())
+L1 = [A]
+
+for _ in range(N):
+    L1.append(int(input()))
+
+L1.sort()
+idx = bisect.bisect_left(L1,B)
+
+if idx == 0:
+    print(f"{min(abs(L1[idx]-B)+1,abs(A-B))}")
+elif idx == N+1:
+    print(f"{min(abs(A-B),abs(L1[idx-1]-B)+1)}")
+else:
+    print(f"{min(abs(L1[idx]-B)+1,abs(A-B),abs(L1[idx-1]-B)+1)}")
